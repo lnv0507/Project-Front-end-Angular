3f350b902b17b128fe14d398937eece6a761a974
lamnguyen-github <18130121@st.hcmuaf.edu.vn>
2021-05-27 09:42:03 +0700

    Update

--- commenau/src/app/app.module.ts
+++ commenau/src/app/app.module.ts
@@ -5,7 +5,6 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { FooterComponent } from './footer/footer.component';
 import { BodyComponent } from './body/body.component';
-import { HeaderMenuComponent } from './header-menu/header-menu.component';
 import { HeaderVipComponent } from './header-vip/header-vip.component';
 
 @NgModule({
@@ -13,7 +12,6 @@ import { HeaderVipComponent } from './header-vip/header-vip.component';
     AppComponent,
     FooterComponent,
     BodyComponent,
-    HeaderMenuComponent,
     HeaderVipComponent
   ],
   imports: [
