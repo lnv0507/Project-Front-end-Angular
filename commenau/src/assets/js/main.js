f0c95e351e4d0cf4375330d959d22e302f1073b6
haole1411 <18130070@st.hcmuaf.edu.vn>
2021-05-29 19:48:42 +0700

    update register

---
+++ commenau/src/assets/js/main.js
@@ -0,0 +1,15 @@
+function mouseOver() {
+    document.getElementById("ic1").style.color = "#3ac5c8";
+  }
+  
+  function mouseOut() {
+    document.getElementById("ic1").style.color = "black";
+  }
+  function mouseOver1() {
+    document.getElementById("ic2").style.color = "#3ac5c8";
+  }
+  function mouseOut1() {
+    document.getElementById("ic2").style.color = "black";
+  }
+  
+  
