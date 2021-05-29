480094355ffdd632f069abeaa77677276f0cadb9
lamnguyen-github <18130121@st.hcmuaf.edu.vn>
2021-05-29 20:30:45 +0700

    Update Tiep tuc

--- commenau/src/app/app.module.ts
+++ commenau/src/app/app.module.ts
@@ -1,24 +1,43 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { Routes, RouterModule } from '@angular/router';
+const routersConfig: Routes = [
+  { path: 'login', component: LoginComponent },
+  { path: 'trangchu', component: HeaderMenuComponent },
+  { path: 'register', component: RegisterComponent },
+  { path: 'forgot', component: ForgotPasswordComponent },
+  { path: 'verify', component: VerifyComponent },
+];
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { FooterComponent } from './footer/footer.component';
 import { BodyComponent } from './body/body.component';
 import { HeaderVipComponent } from './header-vip/header-vip.component';
+import { LoginComponent } from './login/login.component';
+import { RegisterComponent } from './register/register.component';
+import { ForgotPasswordComponent } from './forgot-password/forgot-password.component';
+import { VerifyComponent } from './verify/verify.component';
+import { HeaderMenuComponent } from './header-menu/header-menu.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     FooterComponent,
     BodyComponent,
-    HeaderVipComponent
+    HeaderVipComponent,
+    LoginComponent,
+    RegisterComponent,
+    ForgotPasswordComponent,
+    VerifyComponent,
+    HeaderMenuComponent,
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    RouterModule.forRoot(routersConfig),
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
