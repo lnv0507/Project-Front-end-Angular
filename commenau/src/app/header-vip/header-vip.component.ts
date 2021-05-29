bfc7599dbc19ce0d3f18acabb5f430c69fc9ed7e
lamnguyen-github <18130121@st.hcmuaf.edu.vn>
2021-05-29 19:41:06 +0700

    UPDATE

--- commenau/src/app/header-vip/header-vip.component.ts
+++ commenau/src/app/header-vip/header-vip.component.ts
@@ -7,6 +7,7 @@ import { Component, OnInit, HostListener } from '@angular/core';
 })
 export class HeaderVipComponent implements OnInit {
   header_variable = false;
+  search_click = false;
   constructor() {}
 
   ngOnInit(): void {}
@@ -18,5 +19,31 @@ export class HeaderVipComponent implements OnInit {
     } else {
       this.header_variable = false;
     }
+  }
+  searchClick() {
+    if (this.search_click) {
+      this.search_click = false;
+      if (
+        document.body.scrollTop > 0 ||
+        document.documentElement.scrollTop > 0
+      ) {
+        this.header_variable = true;
+      } else {
+        this.header_variable = false;
+      }
+    } else {
+      this.search_click = true;
+      this.header_variable = true;
+    }
+    // if (this.search_click) {
+    //   document.body.style.backgroundColor = 'gray';
+    //   document.body.style.zIndex = '99999999';
+    //   document.body.style.position = 'absolute';
+    // } else {
+    //   document.body.style.backgroundColor = 'red';
+    // }
+    
+
+    // Note : Làm mờ khi click search mà chưa ra :))
   }
 }
