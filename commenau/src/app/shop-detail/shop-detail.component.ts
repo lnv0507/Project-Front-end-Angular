9cb624a608b09acd33312e1c42e8f0d8575be487
lamnguyen-github <18130121@st.hcmuaf.edu.vn>
2021-06-20 21:44:04 +0700

    Update

--- commenau/src/app/shop-detail/shop-detail.component.ts
+++ commenau/src/app/shop-detail/shop-detail.component.ts
@@ -2,7 +2,6 @@ import { Component, OnInit } from '@angular/core';
 import { ProductsService } from 'src/app/Services/products.service';
 import { ActivatedRoute } from '@angular/router';
 
-
 @Component({
   selector: 'app-shop-detail',
   templateUrl: './shop-detail.component.html',
@@ -10,28 +9,29 @@ import { ActivatedRoute } from '@angular/router';
 })
 export class ShopDetailComponent implements OnInit {
   heartIcon = false;
-  product:any
-  
-  constructor(private serviceProduct: ProductsService, private route : ActivatedRoute) {
-  }
+  product: any;
+  change = false;
+  constructor(
+    private serviceProduct: ProductsService,
+    private route: ActivatedRoute
+  ) {}
 
   ngOnInit(): void {
     const routeParams = this.route.snapshot.paramMap;
     const productIdFromRoute = Number(routeParams.get('productId'));
     this.getProdutById(productIdFromRoute);
   }
-  getProdutById(id: any){
-    this.serviceProduct.getProductById(id).subscribe((res) =>{
+  getProdutById(id: any) {
+    this.serviceProduct.getProductById(id).subscribe((res) => {
       this.product = res;
-    })
+    });
   }
-  
+
   fullHeart() {
     if (this.heartIcon) {
       this.heartIcon = false;
     } else {
       this.heartIcon = true;
-     
     }
   }
 }
