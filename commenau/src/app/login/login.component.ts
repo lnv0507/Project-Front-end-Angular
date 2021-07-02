d2d9d4340a30bd65a5d1c81f17a4050dbde6240e
haole1411 <18130070@st.hcmuaf.edu.vn>
2021-07-02 16:05:41 +0700

    up login

--- commenau/src/app/login/login.component.ts
+++ commenau/src/app/login/login.component.ts
@@ -1,4 +1,7 @@
+import { Route } from '@angular/compiler/src/core';
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { ActivatedRoute, Router } from '@angular/router';
 
 @Component({
   selector: 'app-login',
@@ -6,11 +9,34 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./login.component.scss']
 })
 export class LoginComponent implements OnInit {
+  form!: FormGroup;
+  loading=false;
+  submitted=false;
+  
+  
 
-  constructor() { }
+  constructor(
+    private formBuilder: FormBuilder,
+    private route: ActivatedRoute,
+    private router: Router,
+
+  ) { }
 
   ngOnInit(): void {
+    this.form=this.formBuilder.group({
+      username: ['',Validators.required],
+      password: ['',Validators.required]
+    });
+    
   }
+  get f(){
+    return this.form.controls;
+  }
+  onSubmit(){
+    console.log(this.form);
+
+  }
+  
  
 
 }
