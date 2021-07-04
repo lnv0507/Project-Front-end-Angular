247161eba4867f00176e4169e088b342b0d5a96d
lehuuhuy29 <lehuuhuy2902@gmail.com>
2021-07-04 21:52:19 +0700

    update validator register

--- commenau/src/app/register/register.component.ts
+++ commenau/src/app/register/register.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import {FormControl, FormGroup, Validators} from '@angular/forms'
+import {FormBuilder, FormControl, FormGroup, NgControlStatus, Validators} from '@angular/forms'
 
 @Component({
   selector: 'app-register',
@@ -9,22 +9,39 @@ import {FormControl, FormGroup, Validators} from '@angular/forms'
 export class RegisterComponent implements OnInit {
   formRegister!: FormGroup;
 
-  constructor() { 
-    
+  constructor(private formBuilder :FormBuilder) { 
+    this.formRegister = this.formBuilder.group({
+      name: new FormControl('', Validators.required),
+      email: new FormControl('', [Validators.required, Validators.email]),
+      phone: new FormControl('', [Validators.required, Validators.pattern("^[0-9]+$"), Validators.minLength(10), Validators.maxLength(10)]),
+      password: new FormControl('', Validators.required),
+      re_password: new FormControl('', Validators.required)
+    },
+    {
+      validators : this.MustMatch('password', 're_password')
+    })
   }
 
   ngOnInit(): void {
-    this.formRegister = new FormGroup({
-      name: new FormControl('', Validators.required),
-      email: new FormControl('', [Validators.required, Validators.email]),
-      phone: new FormControl('', Validators.required),
-      password: new FormControl('', Validators.required),
-      re_password: new FormControl('', Validators.required)
-    })
+    
+  }
+
+  MustMatch(controlName: string, matchingControlName: string){
+    return(formGroup : FormGroup) =>{
+      const control = formGroup.controls[controlName];
+      const matchingControl = formGroup.controls[matchingControlName];
+      if(matchingControl.errors && !matchingControl.errors.MustMatch){
+        return
+      }
+      if(control.value !== matchingControl.value){
+        matchingControl.setErrors({MustMatch: true});
+      }else{
+        matchingControl.setErrors(null);
+      }
+    }
   }
   onSubmit(){
     console.log(this.formRegister.value);
-    
   }
   
   
