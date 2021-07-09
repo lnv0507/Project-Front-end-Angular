c05b3dc90aae2e9657d8752176e0a52b102a039a
lehuuhuy29 <lehuuhuy2902@gmail.com>
2021-07-09 22:24:13 +0700

    update validate checkout

--- commenau/src/app/checkout/checkout-detail/checkout-detail.component.ts
+++ commenau/src/app/checkout/checkout-detail/checkout-detail.component.ts
@@ -16,10 +16,10 @@ export class CheckoutDetailComponent implements OnInit {
   ngOnInit(): void {
     this.user = this.userService.user
     this.checkoutForm = this.formBuilder.group({
-      name: new FormControl(this.user.name, Validators.required),
-      address: new FormControl(this.user.address, Validators.required),
-      phone: new FormControl(this.user.phone, [Validators.required, Validators.minLength(10), Validators.maxLength(10)]),
-      email: new FormControl(this.user.email, [Validators.required, Validators.email, gmailValidate])
+      name: [this.user.name, Validators.required],
+      address: [this.user.address, Validators.required],
+      phone: [this.user.phone, [Validators.required, Validators.minLength(10), Validators.maxLength(10), Validators.pattern("^[0-9]+$")]],
+      email: [this.user.email, [Validators.required, Validators.email, gmailValidate]]
     })
   }
 
